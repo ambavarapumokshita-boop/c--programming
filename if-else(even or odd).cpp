a070c503c6b84d8cc69fff6dc8c5a46a1fc740d0
ambavarapumokshita-boop <ambavarapumokshita@gmail.com>
2025-10-28 19:17:41 +0530

    even or odd(if-else)

--- if-else(even or odd).cpp
+++ if-else(even or odd).cpp
@@ -1,26 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
-	printf("enter a&b&c values");
-	scanf("%d%d%d",&a,&b,&c);
-	if(a>b)
-	{
-		if(a>c)
-		printf("a is big");
-		else
-		printf("c is big");
-	}
+	int a;
+	printf("enter a value");
+	scanf("%d",&a");
+	if(a%2==0)
+	printf("a is even");
 	else
-	{
-		if(b>c)
-		printf("b is big");
-		else
-		printf("c is big");
-	}
+	printf("a is odd");
 	return 0;
 	}
 	
+
 
 
 
