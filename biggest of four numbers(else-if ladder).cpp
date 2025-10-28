97762834b0c206f0dff043c82657f853672138c2
ambavarapumokshita-boop <ambavarapumokshita@gmail.com>
2025-10-28 19:02:33 +0530

    Update biggest of four numbers(else-if ladder).cpp

--- biggest of four numbers(else-if ladder).cpp
+++ biggest of four numbers(else-if ladder).cpp
@@ -3,14 +3,15 @@ int main()
 {
 	int a,b,c,d;
 	printf("enter a&b&c&d values");
-	scanf("%d%d%d",&a,&b,&c);
-	if(a>=b&&a>=c&&a>=d)
-	printf("A");
-	else if(b>=c&&b>=d)
-	printf("B");
-	else if(c>=d)
-	printf("C");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	if(a>b&&a>c&&a>d)
+	printf("a is big");
+	else if(b>c&&b>d)
+	printf("b is big");
+	else if(c>d)
+	printf("c is big");
 	else
-	printf("D");
+	printf("d is big");
 	return 0;
 }
+
