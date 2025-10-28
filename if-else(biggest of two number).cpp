9f83e80c26fa6e3dba95d7f18341b17750f760d0
ambavarapumokshita-boop <ambavarapumokshita@gmail.com>
2025-10-28 19:09:07 +0530

    Update if-else(biggest of two number).cpp

--- if-else(biggest of two number).cpp
+++ if-else(biggest of two number).cpp
@@ -4,10 +4,11 @@ int main()
 	int a,b;
 	printf("enter a&b values");
 	scanf("%d%d",&a,&b);
-	if(a<b)
-	printf("a is small");
+	if(a>b)
+	printf("a is big");
 	else
-	printf("b is small");
+	printf("b is big");
 	return 0;
 }
 	
+
