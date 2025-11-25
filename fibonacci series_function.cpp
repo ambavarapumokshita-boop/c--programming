b7393a7d4e5737a47f11ad10a5f73bf97298d073
ambavarapumokshita-boop <ambavarapumokshita@gmail.com>
2025-11-25 15:08:38 +0530

    Add files via upload

---
+++ fibonacci series_function.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void fibo(int);
+int main()
+{
+	int n;
+	printf("enter size value");
+	scanf("%d",&n);
+	fibo(n);
+}
+void fibo(int n)
+{
+	int f=0,s=1,t;
+	printf("%d %d",f,s);
+	t=f+s;
+	while(t<=n)
+	{
+		printf(" %d",t);
+		f=s;
+		s=t;
+		t=f+s;
+	}
+}
